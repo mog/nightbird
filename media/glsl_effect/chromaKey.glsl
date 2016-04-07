0f0e280bc561d120c9aa236ec5d6bb492a96c9a1
mog <mog@trbl.at>
2016-04-08 01:22:02 +0200

    node: mask node that works on texture, renamed previous mask to chroma key

--- media/glsl_effect/chromaKey.glsl
+++ media/glsl_effect/chromaKey.glsl
@@ -9,8 +9,8 @@ uniform vec2 resolution;
 
 uniform sampler2D texture0;
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
+uniform vec3 param0;  //color
 uniform float param1; //delta
 
 #define t time
@@ -19,21 +19,20 @@ uniform float param1; //delta
 
 void main()
 {
-    vec2 uv = c/r;
+  vec2 uv = c/r;
 
     vec4 t0 = texture( texture0, vec2( uv.x, 1.-uv.y ) );
     vec4 t1 = texture( texture1, vec2( uv.x, 1.-uv.y ) );
 
-    vec4 mask = texture( texture2, vec2( uv.x, 1.-uv.y ) );
-
+    vec3 col = param0;//vec4(0.0, 0.0, 0.0, 0.0);
     float delta = param1;//.4;
 
-    if( ((mask.x >= col.x - delta) && (mask.x <= col.x + delta)) &&
-        ((mask.y >= col.y - delta) && (mask.y <= col.y + delta)) &&
-        ((mask.z >= col.z - delta) && (mask.z <= col.z + delta))){
+    if( ((t0.x >= col.x - delta) && (t0.x <= col.x + delta)) &&
+        ((t0.y >= col.y - delta) && (t0.y <= col.y + delta)) &&
+        ((t0.z >= col.z - delta) && (t0.z <= col.z + delta))){
 
-        color = vec4(t1.x, t1.y, t1.z, mask.x);
+        color = t1;
     } else {
-        color = vec4(t0.x, t0.y, t0.z, mask.x);
+        color = t0;
     }
 }
