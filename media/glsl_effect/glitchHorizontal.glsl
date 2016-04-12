8c38e672939cedadffef99413b360544ac192e89
mog <mog@trbl.at>
2016-04-12 09:19:44 +0200

    node: glitchHorizontal now glitches full height

--- media/glsl_effect/glitchHorizontal.glsl
+++ media/glsl_effect/glitchHorizontal.glsl
@@ -23,12 +23,13 @@ void main()
 	vec2 uv = c/r;
 	float my = c.y*5.+t*10.;
 	float mo = noise( floor( ( my+( 2.+sin( my*.1 ) )*noise( t )*20. )*.07 + noise( floor( my*1.1 ) ) ) );
-	float amp = param0 * ( 1.+40.*pow( uv.y*2.-1., 120. ) );
+	float amp = param0 * ( 1.+40.*pow( uv.y, 120. ) );
 	vec3 p = vec3(
 		fract( uv.x+( .4*sin( mo*182.43+floor(t*3.)/3.*.1 ) )*amp ),
 		fract( uv.x+( .4*sin( mo*182.43+floor(t*3.)/3.*.1+.4 ) )*amp ),
 		fract( uv.x+( .4*sin( mo*182.43+floor(t*3.)/3.*.1+.8 ) )*amp )
 	);
+
 	vec3 col = vec3(
 		texture( texture0, vec2( p.x, 1.-uv.y ) ).x,
 		texture( texture0, vec2( p.y, 1.-uv.y ) ).y,
